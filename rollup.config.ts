c3f84e06e939a8d6827321f609f69b606548812b
Jefferson Rylee <rylee.jeff385@gmail.com>
2024-03-10 20:23:06 +0800

    Drop cjs and umd builds

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,7 @@
 import eslint from '@rollup/plugin-eslint';
 import typescript from '@rollup/plugin-typescript';
 import { createRequire } from 'module';
-import { OutputOptions, RollupOptions } from 'rollup';
+import { RollupOptions } from 'rollup';
 import dts from 'rollup-plugin-dts';
 import esbuild from 'rollup-plugin-esbuild';
 import outputSize from 'rollup-plugin-output-size';
@@ -9,22 +9,10 @@ import type Pkg from './package.json';
 
 const require = createRequire(import.meta.url);
 const pkg: typeof Pkg = require('./package.json');
-const name = pkg.name.slice(pkg.name.lastIndexOf('/') + 1);
 const input = 'src/index.ts';
-const inputUmd = 'src/index.umd.ts';
 // skip sourcemap and umd unless production
 const WATCH = process.env.ROLLUP_WATCH === 'true';
 const PROD = !WATCH || process.env.NODE_ENV === 'production';
-
-function umd(options: Partial<OutputOptions>): OutputOptions {
-  return {
-    name,
-    format: 'umd',
-    exports: 'default',
-    sourcemap: PROD,
-    ...options
-  };
-}
 
 function defineConfig(options: (false | RollupOptions)[]) {
   return options.filter((options): options is RollupOptions => !!options);
@@ -33,21 +21,13 @@ function defineConfig(options: (false | RollupOptions)[]) {
 export default defineConfig([
   {
     input,
-    output: [
-      { file: pkg.main, format: 'cjs', exports: 'named', sourcemap: PROD },
-      { file: pkg.module, format: 'esm', exports: 'named', sourcemap: PROD }
-    ],
+    output: {
+      file: pkg.module,
+      format: 'esm',
+      exports: 'named',
+      sourcemap: PROD
+    },
     plugins: [esbuild(), outputSize()]
-  },
-  PROD && {
-    input: inputUmd,
-    output: umd({ file: pkg.unpkg.replace(/\.min\.js$/, '.js') }),
-    plugins: [esbuild(), outputSize()]
-  },
-  PROD && {
-    input: inputUmd,
-    output: umd({ file: pkg.unpkg }),
-    plugins: [esbuild({ minify: true }), outputSize()]
   },
   {
     input,
