af75f72a25020fa75d0a71fb266b3a8e04f49276
Jefferson Rylee <rylee.jeff385@gmail.com>
2025-03-03 21:48:56 +0800

    update spec

--- src/spec/spec.ts
+++ src/spec/spec.ts
@@ -1,7 +1,7 @@
-import { ParseOptions } from '../core/core.types.js';
+import { Options } from '../core/core.types.js';
 import { Spec as SpecClass } from './spec.class.js';
 import { Spec } from './spec.types.js';
 
-export function spec(options: ParseOptions = {}): Spec {
+export function spec(options: Options = {}): Spec {
   return new SpecClass(options);
 }
