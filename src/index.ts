041d3f999d0414ad16f761cf47085744b1b82124
Jefferson Rylee <rylee.jeff385@gmail.com>
2025-03-14 18:48:44 +0800

    remove subdirectory barrel files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
-export * from './core/index.js';
-export * from './schema/index.js';
-export { command as default } from './schema/index.js';
-export * from './types/index.js';
-export * from './utils/index.js';
+export * from './core/error.js';
+export * from './core/split.js';
+export * from './schema/schema.js';
+export { command as default } from './schema/schema.js';
+export * from './schema/schema.types.js';
+export * from './types/node.types.js';
+export * from './types/options.types.js';
+export * from './utils/arg.utils.js';
--- src/types/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './node.types.js';
-export * from './options.types.js';
