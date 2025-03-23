e8d97839d89d415c932c861adefecc6d0e5dc366
Jefferson Rylee <rylee.jeff385@gmail.com>
2025-03-23 21:10:09 +0800

    update split, remove recursive slice

--- src/core/split.ts
+++ src/core/split.ts
@@ -27,29 +27,42 @@ export interface Split {
  * @returns The split result.
  */
 export function split(value: string, matches: string[]): Split {
-  return slice(value, matches, 0, { items: [], values: [], remainder: [] });
-}
-
-function slice(str: string, matches: string[], index: number, s: Split): Split {
-  if (!str) {
-    // do nothing
-  } else if (index < matches.length) {
-    const value = matches[index];
-    // get leftover values (or parts) from recursive calls
-    str.split(value).forEach((part, i) => {
-      // save the match in between parts
-      if (i > 0) {
-        s.items.push({ value, remainder: false });
-        s.values.push(value);
-      }
-
-      part && slice(part, matches, index + 1, s);
-    });
-  } else {
-    // save leftover
-    s.items.push({ value: str, remainder: true });
-    s.remainder.push(str);
+  // like SplitItem but with partial remainder and index
+  interface InternalSplitItem
+    extends Pick<SplitItem, 'value'>,
+      Partial<Pick<SplitItem, 'remainder'>> {
+    index?: number;
+  }
+  interface InternalSplit extends Omit<Split, 'items'> {
+    items: InternalSplitItem[];
   }
 
-  return s;
+  const s: InternalSplit = { items: [], values: [], remainder: [] };
+  value && s.items.push({ value, index: 0 });
+
+  for (let index = 0; index < s.items.length; index++) {
+    const item = s.items[index];
+    if (item.index == null) {
+      s.values.push(item.value);
+    } else if (item.index > matches.length - 1) {
+      // reuse current item object (convert to SplitItem)
+      delete item.index;
+      item.remainder = true;
+      s.remainder.push(item.value);
+    } else {
+      const match = matches[item.index];
+      const items = item.value.split(match).flatMap((part, i) => {
+        const a: InternalSplitItem[] = [];
+        i > 0 && a.push({ value: match, remainder: false } as SplitItem);
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        part && a.push({ value: part, index: item.index! + 1 });
+        return a;
+      });
+
+      // use the same index for the next iteration
+      s.items.splice(index--, 1, ...items);
+    }
+  }
+
+  return s as Split;
 }
