faa82dfb8403f611f4a12dd848727bb6f1ee0af8
Jefferson Rylee <rylee.jeff385@gmail.com>
2025-03-26 20:34:09 +0800

    update recursive example

--- examples/recursive.js
+++ examples/recursive.js
@@ -4,7 +4,7 @@ import { command, getDescendants, isOption, option } from '../lib/index.js';
 /** @returns {never} */
 function help() {
   console.log(
-    'Usage: node examples/recursive.js hello --name world cmd:run command --option value cmd:subcommand --option -- foo bar baz'
+    'Usage: node examples/recursive.js hello --option=1 world cmd:run command --option 2 3 -- --foo bar baz'
   );
   process.exit();
 }
@@ -15,11 +15,6 @@ try {
 } catch (error) {
   console.error(error + '');
   process.exitCode = 1;
-}
-
-/** @param {import('../lib/index.js').Arg} arg */
-function getArgs(arg) {
-  return arg.value != null ? [arg.value] : [];
 }
 
 /** @param {string[]} args */
@@ -38,11 +33,15 @@ function run(args) {
   const handler = arg => {
     if (isOption(arg.key)) {
       // for option ids, remove first 2 hyphens
-      return option({ id: arg.key.replace(/^--?/, ''), args: getArgs(arg) });
-    } else if (arg.key.startsWith(prefix)) {
-      // for command ids, remove 'cmd:' prefix
+      const id = arg.key.replace(/^--?/, '');
+      const args = arg.value != null ? [arg.value] : [];
+      // change max arguments captured if a value is assigned
+      return option({ id, args, max: args.length || null });
+    } else if (arg.value == null && arg.key.startsWith(prefix)) {
+      // commands start with 'cmd:' prefix and should not have assigned value
+      // for command ids, remove prefix
       const id = arg.key.slice(prefix.length);
-      return command({ id, name: id, args: getArgs(arg), init, handler });
+      return command({ id, init, handler });
     }
   };
 
@@ -50,6 +49,12 @@ function run(args) {
   const root = cmd.parse(args);
 
   for (const node of [root].concat(getDescendants(root))) {
-    console.log('%s%s:', '  '.repeat(node.depth), node.id, node.args);
+    console.log(
+      '%s%s%s:',
+      '  '.repeat(node.depth),
+      node.id,
+      node.type === 'value' ? ` (${node.type})` : '',
+      node.args
+    );
   }
 }
