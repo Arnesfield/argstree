82d62163e7502c28bed09426937e7bd5e89e9320
Jefferson Rylee <rylee.jeff385@gmail.com>
2025-04-01 13:24:03 +0800

    add getAncestors spec

---
+++ test/get-ancestors.spec.ts
@@ -0,0 +1,60 @@
+import { expect } from 'chai';
+import command, { getAncestors } from '../src/index.js';
+
+describe('getAncestors', () => {
+  it('should be a function', () => {
+    expect(getAncestors).to.be.a('function');
+  });
+
+  it('should get node ancestors', () => {
+    const cmd = command();
+    cmd.option('--foo');
+    cmd.option('--bar');
+    cmd.command('foo', {
+      init(foo) {
+        foo.command('baz');
+      }
+    });
+    cmd.command('baz', {
+      init(baz) {
+        baz.option('--foo');
+        baz.option('--bar');
+        baz.command('foo', {
+          init(baz) {
+            baz.option('--foo');
+            baz.option('--bar');
+          }
+        });
+      }
+    });
+
+    const args = ([] as string[]).concat(
+      '1',
+      ['--foo', '2', '3'],
+      '--bar',
+      ['baz', '1'],
+      ['--foo', 'baz'],
+      '--bar',
+      ['foo', '1'],
+      ['--bar', '1', '2']
+    );
+    const root = cmd.parse(args);
+
+    const node = root.children[3].children[3].children[1];
+    expect(node.id).to.equal('--bar');
+    expect(node.args).to.deep.equal(['1', '2']);
+
+    const ancestors = getAncestors(node);
+    expect(ancestors).to.be.an('array').that.has.length(3);
+
+    // partial node object check
+    for (const ancestor of ancestors) {
+      expect(ancestor).to.have.property('id');
+      expect(ancestor.args).to.be.an('array');
+    }
+
+    expect(ancestors[0].id).to.equal('foo');
+    expect(ancestors[1].id).to.equal('baz');
+    expect(ancestors[2].id).to.be.null;
+  });
+});
