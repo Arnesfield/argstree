d6bd61a82c3a2d0cb50c2dde7136b5b198cab59e
Jefferson Rylee <rylee.jeff385@gmail.com>
2025-04-05 20:25:42 +0800

    add isOption spec

---
+++ test/is-option.spec.ts
@@ -0,0 +1,38 @@
+import { expect } from 'chai';
+import { isOption } from '../src/index.js';
+
+describe('isOption', () => {
+  it('should be a function', () => {
+    expect(isOption).to.be.a('function');
+  });
+
+  it('should determine if the provided arg looks like an alias or not', () => {
+    // equal signs should still be treated like normal characters
+    const valid = [
+      // alias-likes
+      '-a',
+      '-foo',
+      '-a=value',
+      '-foo=value',
+      '-=',
+      '-=value',
+      // option-likes
+      '--a',
+      '--foo',
+      '--a=value',
+      '--foo=value',
+      '---a',
+      '---foo',
+      '--=',
+      '--=value'
+    ];
+    const invalid = ['a', 'foo', '-', '--', '=', '=value', ''];
+
+    for (const item of valid) {
+      expect(isOption(item)).to.be.true;
+    }
+    for (const item of invalid) {
+      expect(isOption(item)).to.be.false;
+    }
+  });
+});
