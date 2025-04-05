ed05deaf130d1325e2309f90784f1d2f2917fdee
Jefferson Rylee <rylee.jeff385@gmail.com>
2025-04-05 16:30:43 +0800

    update getAncestors spec and extract node test data

--- test/get-ancestors.spec.ts
+++ test/get-ancestors.spec.ts
@@ -1,45 +1,53 @@
 import { expect } from 'chai';
-import command, { getAncestors } from '../src/index.js';
+import command, { getAncestors, Node } from '../src/index.js';
+
+/**
+ * Common node test data for `getAncestors` and `getDescendants`.
+ * @returns The parsed root node.
+ */
+export function getNode(): Node {
+  const cmd = command();
+  cmd.option('--foo');
+  cmd.option('--bar');
+  cmd.command('foo', {
+    init(foo) {
+      foo.command('baz');
+    }
+  });
+  cmd.command('baz', {
+    init(baz) {
+      baz.option('--foo');
+      baz.option('--bar');
+      baz.command('foo', {
+        init(baz) {
+          baz.option('--foo');
+          baz.option('--bar');
+        }
+      });
+    }
+  });
+
+  const args = ([] as string[]).concat(
+    '1',
+    ['--foo', '2', '3'],
+    '--bar',
+    ['baz', '1'],
+    ['--foo', 'baz'],
+    '--bar',
+    ['foo', '1'],
+    ['--bar', '1', '2']
+  );
+
+  return cmd.parse(args);
+}
 
 describe('getAncestors', () => {
   it('should be a function', () => {
     expect(getAncestors).to.be.a('function');
   });
 
-  it('should get node ancestors', () => {
-    const cmd = command();
-    cmd.option('--foo');
-    cmd.option('--bar');
-    cmd.command('foo', {
-      init(foo) {
-        foo.command('baz');
-      }
-    });
-    cmd.command('baz', {
-      init(baz) {
-        baz.option('--foo');
-        baz.option('--bar');
-        baz.command('foo', {
-          init(baz) {
-            baz.option('--foo');
-            baz.option('--bar');
-          }
-        });
-      }
-    });
-
-    const args = ([] as string[]).concat(
-      '1',
-      ['--foo', '2', '3'],
-      '--bar',
-      ['baz', '1'],
-      ['--foo', 'baz'],
-      '--bar',
-      ['foo', '1'],
-      ['--bar', '1', '2']
-    );
-    const root = cmd.parse(args);
-
+  it('should get ancestor nodes', () => {
+    const root = getNode();
     const node = root.children[3].children[3].children[1];
     expect(node.id).to.equal('--bar');
     expect(node.args).to.deep.equal(['1', '2']);
