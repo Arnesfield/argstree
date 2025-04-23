6f2777a5a22c7df87d734bab20341c630831b32f
Jefferson Rylee <rylee.jeff385@gmail.com>
2025-04-23 13:21:14 +0800

    update parse spec (ongoing)

--- test/parse.spec.ts
+++ test/parse.spec.ts
@@ -1,26 +1,7 @@
 import { expect } from 'chai';
-import command, { Node, option } from '../src';
+import command, { NodeType, option } from '../src';
 import { createNode } from './common/create-node';
-
-function expectNode(node: Node, actual: Node) {
-  expect(node).to.have.property('id').that.equals(actual.id);
-  expect(node).to.have.property('name').that.equals(actual.name);
-  expect(node).to.have.property('raw').that.equals(actual.raw);
-  expect(node).to.have.property('key').that.equals(actual.key);
-  expect(node).to.have.property('alias').that.equals(actual.alias);
-  expect(node).to.have.property('type').that.equals(actual.type);
-  expect(node).to.have.property('depth').that.equals(actual.depth);
-  expect(node)
-    .to.have.property('args')
-    .that.is.an('array')
-    .that.deep.equals(actual.args);
-  expect(node)
-    .to.have.property('args')
-    .that.is.an('array')
-    .that.deep.equals(actual.args);
-  expect(node).to.have.property('parent');
-  expect(node).to.have.property('children').that.is.an('array');
-}
+import { expectNode } from './common/expect-node';
 
 describe('parse', () => {
   it('should return the root node', () => {
@@ -35,19 +16,23 @@ describe('parse', () => {
 
   it("should use the provided 'id' and 'name' options", () => {
     let root = command({ id: 'root', name: 'root-name' }).parse([]);
-    let actual = createNode({ id: 'root', name: 'root-name', type: 'command' });
+    let actual = createNode({
+      id: 'root',
+      name: 'root-name',
+      type: 'command'
+    });
     expectNode(root, actual);
 
     root = command({ id: null }).parse([]);
     actual = createNode({ id: null, name: null, type: 'command' });
     expectNode(root, actual);
-  });
 
-  it("should use the provided 'id' and 'name' options for child nodes", () => {
-    let root = command()
+    // for child nodes
+
+    root = command()
       .command('cmd', { id: 'subcmd', name: 'subcmd-name' })
       .parse(['cmd']);
-    let actual = createNode({ type: 'command' });
+    actual = createNode({ type: 'command' });
     expectNode(root, actual);
     expect(root.children).to.have.length(1);
 
@@ -94,5 +79,42 @@ describe('parse', () => {
         depth: 1
       })
     );
+  });
+
+  it('should not be strict by default', () => {
+    // NOTE: strict checks are part of the error spec
+    const fns: { fn: typeof command; type: NodeType }[] = [
+      { fn: command, type: 'command' },
+      { fn: option, type: 'option' }
+    ];
+
+    for (const { fn, type } of fns) {
+      let root = fn().command('cmd').parse(['--foo', 'cmd', '--bar']);
+      expectNode(root, createNode({ type, args: ['--foo'] }));
+      expect(root.children).to.have.length(2);
+      expectNode(
+        root.children[0],
+        createNode({ type: 'value', depth: 1, args: ['--foo'] })
+      );
+      expectNode(
+        root.children[1],
+        createNode({
+          id: 'cmd',
+          name: 'cmd',
+          raw: 'cmd',
+          key: 'cmd',
+          type: 'command',
+          depth: 1,
+          args: ['--bar']
+        })
+      );
+
+      root = fn({ strict: false }).parse(['foo', '--bar', '-baz']);
+      expectNode(root, createNode({ type, args: ['foo', '--bar', '-baz'] }));
+    }
+  });
+
+  it('should be variadic by default', () => {
+    // TODO:
   });
 });
