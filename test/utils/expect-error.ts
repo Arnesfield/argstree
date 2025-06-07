524eac97e73a54fca619433023c301bf8d651d82
Jefferson Rylee <rylee.jeff385@gmail.com>
2025-06-07 13:08:24 +0800

    update expectError test util
    - make args option optional
    - combine both code and message in one assertion for better logging

--- test/utils/expect-error.ts
+++ test/utils/expect-error.ts
@@ -7,20 +7,25 @@ export function expectError<T>(opts: {
   match?: Schema<T>;
   node?: Node<T>;
   message: string;
-  args: string[];
+  args?: string[];
 }): void {
   let error: unknown;
   const cmd = command(opts.options);
   try {
-    cmd.parse(opts.args);
+    cmd.parse(opts.args || []);
   } catch (err) {
     error = err;
   }
 
   expect(error).to.be.instanceof(ParseError);
   if (error instanceof ParseError) {
-    expect(error).to.have.property('code').that.equals(opts.code);
-    expect(error).to.have.property('message').that.equals(opts.message);
+    expect(error).to.have.property('code');
+    expect(error).to.have.property('message');
+    // combine both code and message in one assertion for better logging
+    expect({ code: error.code, message: error.message }).to.deep.equal({
+      code: opts.code,
+      message: opts.message
+    });
     expect(error).to.have.property('node').that.is.an('object');
     expect(error)
       .to.have.property('schema')
